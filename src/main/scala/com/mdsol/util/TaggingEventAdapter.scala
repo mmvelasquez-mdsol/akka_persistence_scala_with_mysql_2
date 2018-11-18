50665bd53e6a09d1e193680745336d962acc6099
mmvelasquez-mdsol <mmvelasquez@mdsol.com>
2018-11-18 14:39:12 +0000

    Fifth commit. Tagging Events

--- src/main/scala/com/mdsol/util/TaggingEventAdapter.scala
+++ src/main/scala/com/mdsol/util/TaggingEventAdapter.scala
@@ -1,16 +1,19 @@
 package com.mdsol.util
 
-import akka.persistence.journal.{Tagged, WriteEventAdapter}
-import com.mdsol.cqrs.event.{CtaCreated, CtaUpdated}
+import akka.persistence.journal.{EventAdapter, EventSeq, Tagged}
+import com.mdsol.cqrs.event.DomainEvent
 
-class TaggingEventAdapter extends WriteEventAdapter {
+class TaggingEventAdapter extends EventAdapter {
 
   override def manifest(event: Any): String = ""
 
   override def toJournal(event: Any): Any = event match {
-    case _: CtaCreated => Tagged(event, Set( "cta"))
-    case _: CtaUpdated => Tagged(event, Set( "cta"))
+    case _: DomainEvent => Tagged(event, Set( "cta"))
     case _ => event
+  }
+
+  override def fromJournal(event: Any, manifest: String): EventSeq = {
+    EventSeq.single(event)
   }
 
 }
